202cbef7ef7133733119591d1c9038abf37edf09
kacper516 <116543741+kacper516@users.noreply.github.com>
2023-03-05 23:23:02 +0100

    Update main.cpp

--- Graph Search/main.cpp
+++ Graph Search/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    // test
     int n = 6;
     int m = 15;
     edge directedGraph[] = {{0, 4}, {0, 5}, {1, 0}, {1, 2}, {1, 4}, {2, 1}, {2, 3}, {2, 4}, {3, 2}, {3, 5}, {4, 0}, {4, 1}, {4, 3}, {5, 3}, {5, 4}};
@@ -11,18 +12,4 @@ int main()
     g.dfs(0);
 
     std::cout << g.connected();
-    /*g.ile_skladowych(0);
-    g.czy_spojny_bfs(0);
-    g.czy_spojny_dfs(0);
-    g.connected_bfs();
-    g.connected_dfs();
-    std::cout<<std::endl;
-    g.czy_spojny(0);
-    g.czy_spojny(1);
-    g.czy_spojny(2);
-    g.czy_spojny(3);
-    g.czy_spojny(4);
-
-    std::cout<<g.connected();*/
-    /*std::cout<<g<<std::endl;*/
 }
